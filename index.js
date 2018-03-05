6830733f8b2bd25d810a6f9bde0fc274c834c658
larry eliemenye <larry.eliemenye@massive.co>
2018-03-05 18:23:19 +0000

    starting to take shape, lets keep going

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const ui = require('./src/ui');
-
-ui.start({ 
-    heartBeatInterval: 3000, 
+const command = require('yargs');
+/**
+  this should be called as a command from the cli
+  ui.start({
+    heartBeatInterval: 3000,
     uiPort: 5550,
-});
+  });
+**/
+
+// this should be called from the app we want to monitor
+exports.agent = require('./src/agent');
+
+
--- src/agent.js
+++ src/agent.js
@@ -1,3 +1,46 @@
-const emitter = require('events').EventEmitter;
+/**
+ * Agent captures
+ * - memory usage stats,
+ * - request stats,
+ * - calls GC at set intervals or lets you do that manually from the UI
+ * - send v8 heap snapshots (v2)
+ *
+ * Charts
+ * - request count vs time
+ * - measure your heap size against time (v2)
+ * - measures your function calls vs heap size (v2)
+ * - measures your time vs function calls (v2)
+ * - Event data is sent to mem-mon via IPC communication.
+ *
+ * Additional Info
+ * The monitor runs on a seperate isolated process so as not to o
+ */
+function recordMemUsageFn(output, options) {
+  const interval = option.interval || 1000
+  setInterval(() => {
+    process.send(process.memoryUsage());
+  }, interval);
+}
 
-function Agent() {}
+function requestGCFn(output, options) {
+  if(!options.exposeGC) {
+    console.log('Garbage Collection is not enabled, please start memwatch-ui with the exposeGC option turn on');
+    return;
+  }
+  return gc();
+}
+
+function recordRequestFn(output, options) {
+
+}
+
+function agent (options) {
+  return { };
+}
+
+exports.recordMemUsage = recordMemUsageFn;
+exports.requestGC =requestGCFn;
+exports.recordRequest = recordRequestFn;
+exports.start = (options) => {
+
+}
